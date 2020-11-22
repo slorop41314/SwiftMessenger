ad06ca1cec8d9ff54af65d9560fc7b8214c49bdd
Albert Stanley <albertstanleyy@gmail.com>
2020-11-22 10:22:37 +0700

    Fix: removing pods from git

--- Pods/Target Support Files/Pods-SwiftMessenger-SwiftMessengerUITests/Pods-SwiftMessenger-SwiftMessengerUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftMessenger_SwiftMessengerUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftMessenger_SwiftMessengerUITests
-@end
--- Pods/Target Support Files/Pods-SwiftMessengerTests/Pods-SwiftMessengerTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftMessengerTests : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftMessengerTests
-@end
